f3ef229b82c080c21f2ebdea230fdc37866fccbd
Caterina062 <flvcrn02p46m208o@studenti.unical.it>
2023-12-28 19:45:26 +0100

    faq

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard_folder/dashboard/dashboard.component';
 import { RegisterComponent } from './register/register.component';
 import { ContactComponent } from './contact/contact.component';
+import { QuestionComponent } from './question/question.component';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent},
   { path: 'contact', component: ContactComponent },
+  { path: 'question', component: QuestionComponent}
 ];
 
 @NgModule({
