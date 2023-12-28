cf14d6b377d6e40c0a72a411c635dfaee4935835
Daniel Curcio <crcdnl02e28m208a@studenti.unical.it>
2023-12-28 09:42:16 +0100

    Initialized Backend

---
+++ backend/EsameWeb24_Backend/src/main/java/com/exam/esameweb24_backend/EsameWeb24BackendApplication.java
@@ -0,0 +1,13 @@
+package com.exam.esameweb24_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsameWeb24BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsameWeb24BackendApplication.class, args);
+    }
+
+}
---
+++ backend/EsameWeb24_Backend/src/test/java/com/exam/esameweb24_backend/EsameWeb24BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.esameweb24_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsameWeb24BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
