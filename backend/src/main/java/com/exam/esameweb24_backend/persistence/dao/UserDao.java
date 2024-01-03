738de0f84fb45cfb8d93c67afc01d4162ae3945f
gianpoo <campanella2001@gmail.com>
2024-01-03 20:41:34 +0100

    Added delete method to UserDao

--- backend/src/main/java/com/exam/esameweb24_backend/persistence/dao/UserDao.java
+++ backend/src/main/java/com/exam/esameweb24_backend/persistence/dao/UserDao.java
@@ -3,9 +3,10 @@ package com.exam.esameweb24_backend.persistence.dao;
 import com.exam.esameweb24_backend.persistence.model.User;
 
 public interface UserDao {
-    public User findByEmail(String passcode);
+    public User findByEmail(String email);
     public User findByToken(String token);
     public User findByPIva(String pIva);
     public Boolean insert(User user);
     public Boolean update(User user);
+    public Boolean delete(User user);
 }
