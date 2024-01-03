702a732695551ff4e9de4cd1813c79cd72dbaf33
peter <petergattuso21@gmail.com>
2024-01-03 16:22:52 +0100

    creazione della classe user e del token per autenticarsi

---
+++ backend/src/main/java/com/exam/esameweb24_backend/controller/AuthToken.java
@@ -0,0 +1,12 @@
+package com.exam.esameweb24_backend.controller;
+
+public class AuthToken {
+    String token;
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
---
+++ backend/src/main/java/com/exam/esameweb24_backend/persistence/model/User.java
@@ -0,0 +1,33 @@
+package com.exam.esameweb24_backend.persistence.model;
+
+public class User {
+    private String email;
+
+    private String password;
+
+    private String pIva;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getpIva() {
+        return pIva;
+    }
+
+    public void setpIva(String pIva) {
+        this.pIva = pIva;
+    }
+}
