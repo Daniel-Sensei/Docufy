82449c99b6a72f4a263096b8bad5e75a70838714
Caterina062 <flvcrn02p46m208o@studenti.unical.it>
2024-01-04 01:33:01 +0100

    modifiche oagina registrati

--- frontend/src/app/model/mocks/mock-corsi.ts
+++ frontend/src/app/model/mocks/mock-corsi.ts
@@ -1,9 +1,9 @@
 import { Corso } from "../Corso";
 
 export const CORSI: Corso[] = [
-    {id: 1, nome: "Corso 1", categoria: "benessere", prezzo: 100, descrizione: "Descrizione 1",  durata: 64, posti: 10, postiDisponibili: 10, esameFinale: true},
-    {id: 2, nome: "Corso 2 nome un po' più lungo", categoria: "cultura", prezzo: 100, descrizione: "Descrizione 2", durata: 64, posti: 10, postiDisponibili: 10, esameFinale: true},
-    {id: 3, nome: "Corso 3 nome molto lungo per controllare le dimensioni", categoria: "Tipo 3", prezzo: 100, descrizione: "Descrizione 3", durata: 64, posti: 10, postiDisponibili: 10, esameFinale: true},
+    {id: 1, nome: "Corso 1", categoria: "marketing", prezzo: 100, descrizione: "Descrizione 1",  durata: 64, posti: 10, postiDisponibili: 10, esameFinale: true},
+    {id: 2, nome: "Corso 2 nome un po' più lungo", categoria: "salute e benessere", prezzo: 100, descrizione: "Descrizione 2", durata: 64, posti: 10, postiDisponibili: 10, esameFinale: true},
+    {id: 3, nome: "Corso 3 nome molto lungo per controllare le dimensioni", categoria: "aggiornamento", prezzo: 100, descrizione: "Descrizione 3", durata: 64, posti: 10, postiDisponibili: 10, esameFinale: true},
     {id: 4, nome: "Corso 4", categoria: "sicurezza", prezzo: 100, descrizione: "Descrizione 4", durata: 64, posti: 10, postiDisponibili: 10, esameFinale: true},
     {id: 5, nome: "Corso 5", categoria: "formazione", prezzo: 100, descrizione: "Descrizione 5", durata: 64, posti: 10, postiDisponibili: 10, esameFinale: true},
 ];
--- frontend/src/app/view/_LOGIN/register/register.component.ts
+++ frontend/src/app/view/_LOGIN/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
@@ -6,5 +9,44 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+  registerForm: FormGroup;
+  model: NgbDateStruct | undefined;
+
+  constructor(
+    private fb: FormBuilder,
+    ) {
+    this.registerForm = this.fb.group({
+      nome: ['', Validators.required],
+      username: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      password_confirmation: ['', Validators.required],
+      terms: [false, Validators.requiredTrue],
+    },); 
+  }
+
+  customValidation(group: FormGroup) {
+    const passwordControl = group.get('password');
+    const passwordConfirmationControl = group.get('password_confirmation');
+    if (passwordControl && passwordConfirmationControl) {
+      const password = passwordControl.value;
+      const passwordConfirmation = passwordConfirmationControl.value;
+      if (password !== passwordConfirmation) {
+        passwordConfirmationControl.setErrors({ 'passwordMismatch': true });
+      } else {
+        // Rimuovi l'errore se la password è valida
+        if (passwordConfirmationControl.hasError('passwordMismatch')) {
+          passwordConfirmationControl.setErrors(null);
+        }
+      }
+    }
+  }
+
+  submitForm() {
+    if(this.registerForm.valid) return ;
+    //stampa a console i valori del form
+    console.log(this.registerForm.value);
+  }
 
 }
