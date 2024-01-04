ae82ad2c82efdb4b896e9df0b7d5eaeb4658e8eb
Caterina062 <flvcrn02p46m208o@studenti.unical.it>
2024-01-04 22:43:23 +0100

    modifica pagina aggiungi azienda

--- frontend/src/app/view/_AZIENDE/add-azienda-modal/add-azienda-modal.component.ts
+++ frontend/src/app/view/_AZIENDE/add-azienda-modal/add-azienda-modal.component.ts
@@ -14,17 +14,16 @@ export class AddAziendaModalComponent {
   model: NgbDateStruct | undefined;
 
   constructor(
-    public activeModal: NgbActiveModal,
+    //public activeModal: NgbActiveModal,
     private fb: FormBuilder,
-    private alert: AlertService,
+    //private alert: AlertService,
     ) {
     this.addAziendaForm = this.fb.group({
       ragione_sociale: ['', Validators.required],
       partita_iva: ['', Validators.required],
       email: ['', [Validators.required, Validators.email, Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$')]],
       telefono: ['', Validators.required],
-      indirizzo: ['',Validators.required],
-      img: ['', ],
+      indirizzo: ['',Validators.required]
     }, { validators: this.customValidation });
   }
   
@@ -38,18 +37,18 @@ export class AddAziendaModalComponent {
       if (partita_ivaControllo && partita_iva.length !== 11) {
         partita_ivaControllo.setErrors({ 'invalidPIvaLength': true });
       } else {
-        if (partita_iva.hasError('invalidCFLength')) {
+        /*if (partita_iva.hasError('invalidCFLength')) {
           partita_iva.setErrors(null);
-        }
+        }*/
       }
 
       //Controlla che la partita iva sia composta solo da numeri
       if (partita_ivaControllo && !/^[0-9]*$/.test(partita_iva)) {
         partita_ivaControllo.setErrors({ 'invalidPIva': true });
       } else {
-        if (partita_iva.hasError('invalidPIva')) {
+        /*if (partita_iva.hasError('invalidPIva')) {
           partita_iva.setErrors(null);
-        }
+        }*/
       }
       
       //Controlla che la partita iva sia valida
@@ -59,12 +58,9 @@ export class AddAziendaModalComponent {
 
 
   submitForm() { 
-    if (this.addAziendaForm.valid) {
-      (this.alert as any).success('Azienda aggiunta con successo');
-      this.activeModal.close(this.addAziendaForm.value);
-    } else {
-      (this.alert as any).error('Errore durante l\'aggiunta dell\'azienda');
-    }
+    console.log(this.addAziendaForm.value);
+    //this.alert.success("Azienda aggiunta con successo");
+    //this.activeModal.close(this.addAziendaForm.value);
   }
 
 }
