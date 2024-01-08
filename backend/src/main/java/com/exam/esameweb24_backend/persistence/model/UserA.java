0f32663480f81f5c2ffc15db0ce8ad3c70f83950
gianpoo <campanella2001@gmail.com>
2024-01-08 18:17:12 +0100

    Changed file update method

--- backend/src/main/java/com/exam/esameweb24_backend/persistence/model/UserA.java
+++ backend/src/main/java/com/exam/esameweb24_backend/persistence/model/UserA.java
@@ -33,7 +33,7 @@ public class UserA extends User{
     public ResponseEntity<String> aggiungiDipendete(MultipartFile json, MultipartFile file) {
 
         // controllo se è stata aggiunta un'immagine
-        boolean thereIsFile = file.getOriginalFilename().isEmpty();
+        boolean thereIsFile = !(file.getOriginalFilename() == null);
 
         // se il json è vuoto allora non può usare il servizio
         if (json.isEmpty()) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
@@ -77,7 +77,7 @@ public class UserA extends User{
     public ResponseEntity<String> modificaDipendente(MultipartFile json, MultipartFile file) {
 
         // controllo se è stata aggiunta un'immagine
-        boolean thereIsFile = !file.isEmpty();
+        boolean thereIsFile = !(file.getOriginalFilename() == null);
 
         // se il json è vuoto allora non può usare il servizio
         if (json.isEmpty()) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
