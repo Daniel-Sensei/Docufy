b71a38e6c1b885ea3495bbc5ddb8e30873765f4f
Daniel Curcio <crcdnl02e28m208a@studenti.unical.it>
2024-01-09 17:52:55 +0100

    Inizio Profilo Azienda

--- frontend/src/app/view/_STATIC/profile/profile.component.ts
+++ frontend/src/app/view/_STATIC/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Azienda } from '../../../model/Azienda';
+import { AziendeService } from '../../../service/aziende/aziende.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
+  azienda!: Azienda;
+
+  constructor(
+    private aziendeService: AziendeService
+  ) {}
+
+  ngOnInit(): void {
+    this.getAzienda();
+  }
+
+  getAzienda() {
+    this.aziendeService.getProfilo().subscribe( azienda => { this.azienda = azienda})
+  }
 
 }
