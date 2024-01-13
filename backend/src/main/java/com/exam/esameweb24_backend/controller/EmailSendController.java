a76ff0d9e123aef9181e0505a858b4427a9f3bc5
Daniel Curcio <crcdnl02e28m208a@studenti.unical.it>
2024-01-13 23:54:17 +0100

    Revert del revert Email

---
+++ backend/src/main/java/com/exam/esameweb24_backend/controller/EmailSendController.java
@@ -0,0 +1,25 @@
+package com.exam.esameweb24_backend.controller;
+
+
+import com.exam.esameweb24_backend.controller.service.EmailService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/mail")
+public class EmailSendController {
+
+    private EmailService emailService;
+
+    public EmailSendController(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @PostMapping("/send")
+    public String sendMail(String to, String[] cc, String subject, String body){
+        return emailService.sendMail(to, cc, subject, body);
+    }
+}
