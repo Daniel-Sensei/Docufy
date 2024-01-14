b9b512896cc01119181bfd887e401a76628ee9d0
Caterina062 <flvcrn02p46m208o@studenti.unical.it>
2024-01-15 00:08:00 +0100

    pagina no-data

---
+++ frontend/src/app/view/GENERIC/nessun-componente/nessun-componente.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nessun-componente',
+  templateUrl: './nessun-componente.component.html',
+  styleUrl: './nessun-componente.component.css'
+})
+export class NessunComponenteComponent {
+
+}
