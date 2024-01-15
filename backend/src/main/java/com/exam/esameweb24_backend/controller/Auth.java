5091d03dd79bd3de6e66ed09a0562072f5a8a773
Daniel Curcio <crcdnl02e28m208a@studenti.unical.it>
2024-01-15 01:06:16 +0100

    Added Session (Servlet)

--- backend/src/main/java/com/exam/esameweb24_backend/controller/Auth.java
+++ backend/src/main/java/com/exam/esameweb24_backend/controller/Auth.java
@@ -3,17 +3,18 @@ package com.exam.esameweb24_backend.controller;
 import com.exam.esameweb24_backend.persistence.DBManager;
 import com.exam.esameweb24_backend.persistence.model.User;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin(value = "http://localhost:4200", allowCredentials = "true")
 public class Auth {
 
     @PostMapping("/login")
-    public AuthToken login(@RequestBody User user){
+    public AuthToken login(@RequestBody User user, HttpServletRequest req){
         User storedUser = DBManager.getInstance().getUserDao().findByEmail(user.getEmail());
         if(storedUser == null){
             return null;
@@ -22,11 +23,29 @@ public class Auth {
             return null;
         }
         user.setPIva(storedUser.getPIva());
+
+        // Session
+        HttpSession session = req.getSession();
+        session.setAttribute("user", user);
+
         if(DBManager.getInstance().getUserDao().isConsultant(storedUser)) {
             return generateToken(user, "C");
         }
         else {
             return generateToken(user, "A");
+        }
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(HttpServletRequest req) {
+        HttpSession session = req.getSession(false); // Ottieni la sessione senza crearne una nuova se non esiste
+
+        if (session != null) {
+            session.invalidate(); // Invalida la sessione
+            System.out.println("Sessione invalidata");
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build(); // Non c'è nessuna sessione da invalidare
         }
     }
 
